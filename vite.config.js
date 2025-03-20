94a6b2e2d1a06d255c0d08c58a4c3d595355f19c
ayush18pop <ayushsp05@gmail.com>
2025-03-20 20:42:22 +0530

    some patches

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "react-router-dom": path.resolve(
+        __dirname,
+        "node_modules/react-router-dom"
+      ),
+    },
+  },
 });
